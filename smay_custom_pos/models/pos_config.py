d2db9945740f8d7595c4da8bbd49237df2e03e17
GeraOne <odoo.supermay@gmail.com>
2020-05-25 14:43:18 -0500

    test_custom pos

--- smay_custom_pos/models/pos_config.py
+++ smay_custom_pos/models/pos_config.py
@@ -23,10 +23,10 @@ class PosConfig(models.Model):
 
     # @api.api
     def open_session_cb(self):
-        if not self.env.user.x_studio_sucursal_id:
+        if not self.env.user.sucursal_id:
             raise UserError('No puedes abrir el punto de venta porque no tienes sucursal asignada')
-        if self.env.user.x_studio_sucursal_id.id != self.sucursal_id.id:
+        if self.env.user.sucursal_id.id != self.sucursal_id.id:
             raise UserError(
                 'No puedes abrir el punto de venta porque pertenece a otra sucursal. Tu sucursal asignada es ' + str(
-                    self.env.user.x_studio_sucursal_id.name) + ' y del punto de venta ' + str(self.sucursal_id.name))
+                    self.env.user.sucursal_id.name) + ' y del punto de venta ' + str(self.sucursal_id.name))
         return super(PosConfig, self).open_session_cb()
