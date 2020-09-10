19f788b793716766714912cf3fb186fac4e76171
GeraOne <odoo.supermay@gmail.com>
2020-09-10 13:10:59 -0500

    compras

--- smay_purchases_users/models/models.py
+++ smay_purchases_users/models/models.py
@@ -226,9 +226,10 @@ class SmayPurchasesOrder(models.Model):
 
                 email_to = ''
                 for user in self.env.user.company_id.x_notification_partner_ids:
-                    _logger.warning('COREEEEEEEOOOO')
-                    _logger.warning(str(user))
-                    email_to += user.email + ';'
+                    if user.email:
+                        _logger.warning('COREEEEEEEOOOO')
+                        _logger.warning(str(user))
+                        email_to += user.email + ';'
 
                 mail = self.env['mail.mail']
                 data = {}
