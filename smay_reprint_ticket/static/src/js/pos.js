4a89902611b0a31ccb11acdf7ca127faa54feba4
GeraOne <odoo.supermay@gmail.com>
2020-09-14 10:25:16 -0500

    Impresion

--- smay_reprint_ticket/static/src/js/pos.js
+++ smay_reprint_ticket/static/src/js/pos.js
@@ -228,6 +228,7 @@ odoo.define('smay_reprint_ticket.smay_reprint_ticket', function(require){
                             self.gui.show_screen('receipt',{reprint:'true'})
                             $('.pos-receipt').html(QWeb.render('ReprintPosTicket' ,{widget:self,receipt:receipt}));
                             this.print()
+                            console.log(this)
 
                         }).catch(function(){
                     });
