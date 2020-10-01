ecd623de60d3aa6af92a91215b1db98a64683e9b
GeraOne <odoo.supermay@gmail.com>
2020-10-01 13:59:20 -0500

    Facturacion

--- smay_pos_global_invoice/models/global_invoice_wizard.py
+++ smay_pos_global_invoice/models/global_invoice_wizard.py
@@ -409,7 +409,7 @@ class GlobalInvoiceWizard(models.TransientModel):
         lineas_borrar = []
         for line in data_invoice['line_ids']:
             _logger.warning('LINEAAAAAA'+str(line))
-            if line[2]['name'] and not line[2]['product_id'] and line[2]['credit'] and line[2]['tax_base_amount'] == 0:
+            if line[2]['name']:# and not line[2]['product_id'] and line[2]['credit'] and line[2]['tax_base_amount'] == 0:
                 _logger.warning('GEGEGEGEGEG')
                 _logger.warning(str(line))
                 lineas_borrar.append(line)
