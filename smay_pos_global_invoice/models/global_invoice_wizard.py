8a9f9471f528be717d7bda2d410ec61b5ec207bb
GeraOne <odoo.supermay@gmail.com>
2020-10-04 12:53:34 -0500

    Factura Global

--- smay_pos_global_invoice/models/global_invoice_wizard.py
+++ smay_pos_global_invoice/models/global_invoice_wizard.py
@@ -808,7 +808,7 @@ class GlobalInvoiceCreditNoteWizard(models.TransientModel):
 
     def add_tax_line(self,data_invoice,invoice_id):
         #aqui
-        taxe_ids = self.env['account.move.line'].search([('product_id','=',None),('tax_line_id','!=',None)])
+        taxe_ids = self.env['account.move.line'].search([('product_id','=',None),('tax_line_id','!=',None),('move_id','=',invoice_id)])
         for tax in taxe_ids:
             _logger.warning('TAXXXXXXXXXXXX')
             _logger.warning(str(tax.name))
