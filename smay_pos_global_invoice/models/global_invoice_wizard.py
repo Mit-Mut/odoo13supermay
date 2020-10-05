bca39c8282f4f86e0e133abdb0b38c13e3d51819
GeraOne <odoo.supermay@gmail.com>
2020-10-05 10:17:14 -0500

    Factura Global

--- smay_pos_global_invoice/models/global_invoice_wizard.py
+++ smay_pos_global_invoice/models/global_invoice_wizard.py
@@ -962,8 +962,9 @@ class GlobalInvoiceCreditNoteWizard(models.TransientModel):
                     if li[2]['name'] == False:
                         price_unit_aux = round(abs(li[2]['price_unit']), 2)
                         debit_aux = li[2]['debit']
-                        li[2]['price_unit'] = - round((price_unit_aux + amount_total), 2)
-                        li[2]['debit'] = round(debit_aux + amount_total, 2)
+                        #li[2]['price_unit'] = - round((price_unit_aux + amount_total), 2)
+                        #li[2]['debit'] = round(debit_aux + amount_total, 2)
+                        li[2]['credit'] = round(debit_aux + amount_total, 2)
                         break
 
                 impuesto = self.env['account.tax'].browse(order_taxes.get(order_tax))
