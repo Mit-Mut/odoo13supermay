bab3c57acf25dd328d4d11b3144ba706bd155d7e
GeraOne <odoo.supermay@gmail.com>
2020-10-19 16:39:11 -0500

    Factura global

--- smay_pos_global_invoice/models/global_invoice_wizard.py
+++ smay_pos_global_invoice/models/global_invoice_wizard.py
@@ -703,7 +703,7 @@ class GlobalInvoiceCreditNoteWizard(models.TransientModel):
         for order in orders:
             _logger.warning(str(order.pos_reference))
             invoice_id = self.env['account.move.line'].search(
-                [('name', 'like', order.pos_reference.replace('Predido ', '').replace('Orden ', '')),
+                [('name', 'like', order.pos_reference),
                  ('move_id.type', '=', 'out_invoice'),
                  ],limit=1, order ='id asc').move_id.id
             _logger.warning(str(invoice_id))
