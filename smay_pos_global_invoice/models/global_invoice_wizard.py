c6cecae8d9dcafc68af34ebc491f2329dd84282c
GeraOne <odoo.supermay@gmail.com>
2020-10-20 09:46:08 -0500

    Factura global

--- smay_pos_global_invoice/models/global_invoice_wizard.py
+++ smay_pos_global_invoice/models/global_invoice_wizard.py
@@ -826,7 +826,9 @@ class GlobalInvoiceCreditNoteWizard(models.TransientModel):
 
                 _logger.warning('Se anexa el siuiente registro')
                 _logger.warning(str(tax_line))
-                data_invoice['line_ids'].append(tuple(tax_line))
+                data_invoice['line_ids'].append(tax_line)
+                _logger.warning('impueto agrgado')
+                _logger.warning(str(data_invoice))
         return  data_invoice
 
     def _add_invoice_lines(self, data_invoice, invoice_id, order_ids):
