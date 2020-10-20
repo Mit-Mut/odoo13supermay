181086d5c64e38590a2d5796a93709a36d9c2655
GeraOne <odoo.supermay@gmail.com>
2020-10-20 09:37:19 -0500

    Factura global

--- smay_pos_global_invoice/models/global_invoice_wizard.py
+++ smay_pos_global_invoice/models/global_invoice_wizard.py
@@ -724,7 +724,7 @@ class GlobalInvoiceCreditNoteWizard(models.TransientModel):
             _logger.warning('Aqui empiezo la factura' + str(factura_id))
             data_invoice = self.prepare_invoice(factura_id)
             data_invoice['line_ids'].append(self._get_line_totals(factura_id))
-            self.add_tax_line(data_invoice, invoice_id)
+            data_invoice= self.add_tax_line(data_invoice, invoice_id)
             _logger.warning(str('tetetetetetet'))
             _logger.warning(str(invoices_to_refund))
             data_invoice = self._add_invoice_lines(data_invoice, invoice_id, invoices_to_refund[factura_id])
