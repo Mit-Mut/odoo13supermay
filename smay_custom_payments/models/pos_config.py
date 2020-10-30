845ef5cea6db81c14fc2ffefe6995486172ee6ab
GeraOne <odoo.supermay@gmail.com>
2020-10-30 15:14:37 -0600

    smay_custom_payments

--- smay_custom_payments/models/pos_config.py
+++ smay_custom_payments/models/pos_config.py
@@ -6,6 +6,6 @@ from odoo import models, fields, api
 class smay_custom_payments_PosConfig(models.Model):
     _inherit = 'pos.config'
 
-    multiple_payments = fields.Boolean(string="Permite multiples metodos de pago", default=True,
+    multiple_payments = fields.Boolean(string="Multiples metodos de pago", default=True,
                                        help="Permite que se puedan seleccionar multiples metodos de pago "
                                             "para saldar la orden.")
