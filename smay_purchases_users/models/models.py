2dabe677de1f0dd94262bdd9ad17b87928ee503c
GeraOne <odoo.supermay@gmail.com>
2020-11-19 09:37:26 -0600

    Compras

--- smay_purchases_users/models/models.py
+++ smay_purchases_users/models/models.py
@@ -344,12 +344,14 @@ class SmayPurchasesOrder(models.Model):
 
             # for product in products:
             for product in products:
+                fecha = ''
+                if product.product_tmpl_id.x_fecha_actualizacion_precios:
+                    fecha=product.product_tmpl_id.x_fecha_actualizacion_precios
                 data['body_html'] += "<tr>\
                     <td style='border:1px solid white;border-bottom:1px solid black;border-right:1px solid black;padding-left:5px'>" + product.product_tmpl_id.name + "</td>\
                     <td style='border:1px solid white;border-bottom:1px solid black;border-right:1px solid black;padding-left:5px'>" + product.x_purcharse_change_price + "</td>\
-                    <td style='border:1px solid white;border-bottom:1px solid black;border-right:1px solid black;padding-left:5px'>" + product.product_tmpl_id.x_fecha_actualizacion_precios + "</td>\
-                    <td style='border:1px solid white;border-bottom:1px solid black;border-right:1px solid black;text-align:right;padding-right:5px'> $" + '{:,.2f}'.format(
-                    product.product_tmpl_id.x_last_price) + "</td>\
+                    <td style='border:1px solid white;border-bottom:1px solid black;border-right:1px solid black;padding-left:5px'>" + fecha + "</td>\
+                    <td style='border:1px solid white;border-bottom:1px solid black;border-right:1px solid black;text-align:right;padding-right:5px'> $" + '{:,.2f}'.format(product.product_tmpl_id.x_last_price) + "</td>\
                     <td style='border:1px solid white;border-bottom:1px solid black;text-align:right;padding-right:5px'><b> $" + '{:,.2f}'.format(
                     product.list_price) + "</b></td></tr>"
             data['body_html'] += '</table>'
