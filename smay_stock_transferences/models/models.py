9bf9727867274ab85e667c254454c66981cc46dc
GeraOne <odoo.supermay@gmail.com>
2020-11-21 13:14:47 -0600

    Transferencias

--- smay_stock_transferences/models/models.py
+++ smay_stock_transferences/models/models.py
@@ -53,13 +53,15 @@ class smayTransferencesStockPicking(models.Model):
         readonly=True, required=True,
         states={'draft': [('readonly', False)]})
 
+    move_ids_without_package = fields.One2many('stock.move', 'picking_id', string="Stock moves not in package",
+                                               compute='_compute_move_without_package',
+                                               default=lambda  self: self.get_qty(),
+                                               inverse='_set_move_without_package')
+
     @api.onchange('move_ids_without_package')
-    def _change_productttt(self):
-        _logger.warning('WWWWWWWWWWWWWWWWWWW'+str(self.move_ids_without_package))
-        for move in self.move_ids_without_package:
-            if move.product_uom_qty==0:
-                _logger.warning(str(move.product_id))
-                move.product_uom_qty =100
+    def get_qty(self):
+        _logger.warning('WWWWWWWWWWWWWWWWWWW' + str(self.move_ids_without_package))
+        return 1
 
     @api.model
     def _default_location(self):
