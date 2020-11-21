4c9ca7abf87cf0d578e3cb94766024732a38a94e
GeraOne <odoo.supermay@gmail.com>
2020-11-21 12:32:19 -0600

    Transferencias

--- smay_stock_transferences/models/models.py
+++ smay_stock_transferences/models/models.py
@@ -53,7 +53,7 @@ class smayTransferencesStockPicking(models.Model):
         readonly=True, required=True,
         states={'draft': [('readonly', False)]})
 
-    @api.onchange('move_line_ids.product_id,move_line_ids_without_package.product_id')
+    @api.onchange('move_line_ids,move_line_ids_without_package')
     def _change_productttt(self):
         _logger.warning('WWWWWWWWWWWWWWWWWWW')
 
